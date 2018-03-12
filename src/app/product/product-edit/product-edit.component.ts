4b7794ac08229ef19f98aae7a6415d1a0b63f8a3
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2018-03-12 11:03:46 +0530

    edit form starting

--- src/app/product/product-edit/product-edit.component.ts
+++ src/app/product/product-edit/product-edit.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+// GET /api/products/34345
+// Edit existing data (id exist) or create new one (id is not present)
+
+import {ActivatedRoute, // read url params
+        Router  // helps to navigate between pages
+        } from '@angular/router';
+import { ProductService } from '../services/product.service';
+import { Product } from '../models/product';
+import { Brand } from '../models/brand';
+
 @Component({
   selector: 'app-product-edit',
   templateUrl: './product-edit.component.html',
@@ -7,9 +17,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductEditComponent implements OnInit {
 
-  constructor() { }
+  product: Product = new Product(); // create
+  brands: Brand[] = [];
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private productService: ProductService
+            ) { }
 
   ngOnInit() {
+     const id = this.route.snapshot.params['id'];
+     console.log("ID is", id);
+
+     if (id) { // edit part
+        this.productService.getProduct(id)
+            .subscribe (product => {
+              this.product = product;
+            });
+     }
+
+     this.productService.getBrands()
+        .subscribe( brands => {
+          this.brands = brands;
+        });
+
+  }
+
+  gotoList() {
+    this.router.navigateByUrl("/products"); //products/list
   }
  
 }
--- src/app/product/services/product.service.ts
+++ src/app/product/services/product.service.ts
@@ -5,6 +5,7 @@ import { Product } from '../models/product';
 import { Observable } from 'rxjs/Observable';
 
 import { environment } from '../../../environments/environment';
+import { Brand } from '../models/brand';
 
 console.log("ENV", environment);
 
@@ -14,9 +15,25 @@ export class ProductService {
   // DI
   constructor(private http: HttpClient) { }
 
+  // GET /api/products
   getProducts(): Observable<Product[]> {
     return this.
            http.get<Product[]>(`${environment.apiEndPoint}/api/products`);
   }
+
+   // GET /api/brands
+  getBrands(): Observable<Brand[]> {
+    return this.
+           http.get<Brand[]>(`${environment.apiEndPoint}/api/brands`);
+  }
+
+
+  // GET /api/products/12345
+  getProduct(id): Observable<Product> {
+    return this.
+           http.get<Product>(`${environment.apiEndPoint}/api/products/${id}`);
+  }
+
+
 
 }
